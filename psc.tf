34a65ec18cc7d0ae4a429642582669946482727d
Silpashree <silpashree@bootlabstech.com>
2022-09-29 15:58:52 +0530

    initial commit

---
+++ psc.tf
@@ -0,0 +1,15 @@
+resource "google_compute_global_address" "default" {
+  address            = var.address
+  address_type       = var.address_type
+  name               = var.address_name
+  network            = var.network
+  prefix_length      = var.subnet_mask
+  project            = module.project.project_id
+  purpose            = var.purpose
+}
+resource "google_service_networking_connection" "psc" {
+  depends_on              = [google_compute_global_address.default]
+  network                 = var.network
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.default.name]
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "address" {
+  type        = string
+  description = "the ip of the resource"
+}
+variable "address_type" {
+  type        = string
+  description = "the address type of the resource"
+}
+variable "address_name" {
+  type        = string
+  description = "the name of the resource"
+}
+variable "network" {
+  type        = string
+  description = "the network within which the resource will be created"
+}
+variable "subnet_mask" {
+  type        = number
+  description = "the subnet mask for the CIDR of the resource"
+}
+variable "purpose" {
+  type        = string
+  description = "the purpose of this resource within the network."
+}
